267b448f8ca33fd0389c0f833f246a795206b213
tianzx <tianzx@aliyun.com>
2023-04-13 13:07:01 +0800

    gogogo

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,27 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder, dev::Server};
+use std::net::TcpListener;
+use actix_web::{dev::Server, web, App, HttpResponse, HttpServer, Responder};
+
+#[derive(serde::Deserialize)]
+struct FormData {
+    email:String,
+    name:String
+}
 
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
-pub fn run() -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .bind("127.0.0.1:8000")?
-        .run();
-    Ok(server)    
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse { 
+    HttpResponse::Ok().finish() 
+}
+
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check)) // A new entry in our routing table for POST /subscriptions requests
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
+    Ok(server)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+use std::net::TcpListener;
+
 use rust_web::run;
 
 #[tokio::main]
 async fn main() ->std::io::Result<()> {
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind random port"); 
+    // let port= listener.local_addr().unwrap().port();
+    run(listener)?.await
 }
